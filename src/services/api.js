0c8ee577f2419a3e9c07eda01478ce94f8c5084c
fabianoandrad <affatecnologia@gmail.com>
2021-02-24 20:32:48 -0300

    comunicaçao com back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://https://calculo-de-horas-back-end.vercel.app",
 });
 
 export default api;
