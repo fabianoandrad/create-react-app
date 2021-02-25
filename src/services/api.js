feb7539e0cf35099c56b5fae0b1d5896bb1371f4
fabianoandrad <affatecnologia@gmail.com>
2021-02-24 21:59:23 -0300

    port 8080

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://calculo-de-horas-back-end.vercel.app:3000",
+  baseURL: "https://calculo-de-horas-back-end.vercel.app:8080",
 });
 
 export default api;
