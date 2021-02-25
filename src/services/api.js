3035c9e1176747c56cbb88baa287c71533aaf80a
fabianoandrad <affatecnologia@gmail.com>
2021-02-25 00:02:18 -0300

    new test

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://back-end-proveu.vercel.app",
+  baseURL: "https://back-end-proveu.vercel.app/",
 });
 
 export default api;
