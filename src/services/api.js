0d4bcc0d582001a6f45fdd1f0271e19e96b76389
fabianoandrad <affatecnologia@gmail.com>
2021-02-24 23:48:21 -0300

    trantando erro de pagina test 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://back-end-proveu.vercel.app",
+  baseURL: "https://calculo-de-horas-back-end.vercel.app:8080",
 });
 
 export default api;
