5dde5c889f1e8ec4dd25b209fa6d0f5fa86891e1
fabianoandrad <affatecnologia@gmail.com>
2021-02-25 17:25:29 -0300

    link origin

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://back-end-calc-hcjdqi7oz-fabianoandrad.vercel.app",
+  baseURL: "https://back-end-calc.vercel.app",
 });
 
 export default api;
