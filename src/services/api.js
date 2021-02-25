5846725f83b880ef1c0403a2106688e868a9fcbb
fabianoandrad <affatecnologia@gmail.com>
2021-02-24 23:35:00 -0300

    alteração do link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://back-end-proveu.vercel.app/api/date",
+  baseURL: "https://back-end-proveu.vercel.app",
 });
 
 export default api;
