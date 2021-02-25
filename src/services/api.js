06dd59b9335e8ff24501a13ca494da7df87826f6
fabianoandrad <affatecnologia@gmail.com>
2021-02-24 22:06:49 -0300

    umbler

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://calculo-de-horas-back-end.vercel.app:8080",
+  baseURL: "http://calculatorbd-com.umbler.net/:8080",
 });
 
 export default api;
