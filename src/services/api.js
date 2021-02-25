6120a7d5c15b45aedc507eb59c05d4883bbacea8
fabianoandrad <affatecnologia@gmail.com>
2021-02-24 21:09:49 -0300

    front porta 3000

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://calculo-de-horas-back-end.vercel.app",
+  baseURL: "https://calculo-de-horas-back-end.vercel.app:3000",
 });
 
 export default api;
