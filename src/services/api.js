c151b52eea5d50c2c5b86ce41dd14acc9a231d70
fabianoandrad <affatecnologia@gmail.com>
2021-02-28 19:50:41 -0300

    test lhoroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://back-end-calc.vercel.app",
+  baseURL: "https://calculo-de-horas-back-end.herokuapp.com",
 });
 
 export default api;
