94729da73a8cd3032e45ec13a405d13292f661df
fabianoandrad <affatecnologia@gmail.com>
2021-02-28 19:02:14 -0300

    testtt

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
       <label>Minuto<input id="minOut" type="number" min="0" max="59" name="MinOut" /> </label>
 
       <button type="submit">
-        Cálcular hora
+        Cálcular horas
       </button>
       <span> </span>
       <button type="button" onClick={handleReset}>
